efa01c085bf0d5d360a84445480214433524ed91
aakritimehraa <aakritimehrasa@gmail.com>
2021-06-26 18:56:25 +0530

    landing page commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './App.css'
+import Header from './Header'
 
 function App() {
   return (
-    <div>
-      
+    <div className='app'>
+        <Header />
+
+  <button>shop now</button>
+  
     </div>
   )
 }
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+    return (
+                <div className='container'>
+
+<div className='header'>
+
+<h2>Ace furnitures</h2> 
+ <p>home</p>
+  <p>about </p>
+   <p>home</p>
+
+</div>
+
+<img src='https://banner2.cleanpng.com/20180313/frw/kisspng-table-nightstand-couch-furniture-living-room-neutral-gray-decorative-fashion-sofa-5aa7837f1f2405.5890853615209276151276.jpg' />
+<h1>Exclusive furniture , Comfort furniture <br></br>at affodable prices</h1>
+
+</div>
+
+    )
+}
+
+export default Header
